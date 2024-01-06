55f26b9e45fadabcdbcb8d50abe611bfb9e8a1e3
jdamasceno <jonasmichael2021@outlook.com>
2024-01-06 19:46:02 -0300

    feat: implementa o migration para a tabela users

---
+++ src/migrations/20240106223820-create-User.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    });
+
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, _Sequelize) => await queryInterface.dropTable('users'),
+};
