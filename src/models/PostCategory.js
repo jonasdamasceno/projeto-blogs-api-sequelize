febd6d2d51f457bf3a9c10b82e4f6293b8cdc830
jdamasceno <jonasmichael2021@outlook.com>
2024-02-04 21:11:17 -0300

    requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define(
+        'PostCategory',
+        {
+            postId: { type: DataTypes.INTEGER, primaryKey: true },
+            categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+        },
+        {
+            timestamps: false,
+            underscored: true,
+        },
+    );
+
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+            as: 'categories',
+            through: PostCategory,
+            foreignKey: 'postId',
+            otherKey: 'categoryId',
+        });
+
+        models.Category.belongsToMany(models.BlogPost, {
+            as: 'posts',
+            through: PostCategory,
+            foreignKey: 'categoryId',
+            otherKey: 'postId',
+        });
+    };
+
+    return PostCategory
+};
